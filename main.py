86f283db508815201aed548200d18d0a9c50341d
Karthickravi138 <ravikarthik138@gmail.com>
2025-08-06 09:26:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from utils.math_utils import add, multiply
+from utils.string_utils import reverse_string
+
+if __name__ == "__main__":
+    print("Addition:", add(10, 5))
+    print("Multiplication:", multiply(3, 4))
+    print("Reversed:", reverse_string("Karthik"))
