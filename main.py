7e6ebdca775991cbb27556616a0971c8e15ec190
Karthickravi138 <ravikarthik138@gmail.com>
2025-08-06 09:56:50 +0530

    updated subtraction function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from utils.math_utils import add, multiply
+from utils.math_utils import add, multiply, sub
 from utils.string_utils import reverse_string
 
 if __name__ == "__main__":
     print("Addition:", add(10, 5))
     print("Multiplication:", multiply(3, 4))
     print("Reversed:", reverse_string("Karthik"))
+    print("Subtraction:", sub(10, 5))
